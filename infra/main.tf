058853bf2b94bfde7517de4d4fc621e7403bd99a
Mario G <mariojgintili@gmail.com>
2020-05-10 08:09:14 +0100

    page 55 - provision an autoscaling group with a load balancer slapped on top

--- infra/main.tf
+++ infra/main.tf
@@ -7,10 +7,12 @@ variable "server_port" {
   description = "The port that the server will use for HTTP requests"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-40d28157"
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "example" {
+  image_id               = "ami-40d28157"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.example.id}"]
+  security_groups = ["${aws_security_group.example.id}"]
 
   user_data = <<-EOF
    #!/bin/bash
@@ -18,13 +20,32 @@ resource "aws_instance" "example" {
    nohup busybox httpd -f -p "${var.server_port}"
   EOF
 
-  tags = {
-    Name = "example-1"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones = data.aws_availability_zones.all.names
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+
+  min_size = 2
+  max_size = 4
+
+  tag {
+    key = "Name"
+    value = "terraform-autoscaling-group-example"
+    propagate_at_launch = true
   }
 }
 
 resource "aws_security_group" "example" {
   name = "terraform-example-instance"
+  # description = "This group allows incoming TCP requests on port 8080 from CIDR block 0.0.0.0/0. In english: this group allows all incoming requests on port 8080 from any IP"
 
   ingress {
     from_port   = var.server_port
@@ -32,9 +53,53 @@ resource "aws_security_group" "example" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of my box"
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  description = "Allows incoming requests on port 80, the default for HTTP to the AWS autoscaling group"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # allows all incoming IPs
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "example" {
+  name = "tf-elb-example"
+  availability_zones = data.aws_availability_zones.all.names
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = 80 # the default port of HTTP
+    lb_protocol = "http"
+    instance_port = var.server_port
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+output "elb_dns_name" {
+  value = aws_elb.example.dns_name
+  description = "The public IP address of my boxes"
 }
