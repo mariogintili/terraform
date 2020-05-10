8c92f07db27373725291d12e43807e2b4cbb5859
Mario G <mariojgintili@gmail.com>
2020-05-10 07:04:50 +0100

    use variables and outputs - page 48

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+  default     = 8080
+  description = "The port that the server will use for HTTP requests"
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-40d28157"
   instance_type          = "t2.micro"
@@ -10,7 +15,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
    #!/bin/bash
    echo "Hello, Terraform 🥶" > index.html
-   nohup busybox httpd -f -p 8080
+   nohup busybox httpd -f -p "${var.server_port}"
   EOF
 
   tags = {
@@ -22,9 +27,14 @@ resource "aws_security_group" "example" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port  = 8080
-    to_port    = 8080
-    protocol   = "tcp"
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of my box"
 }
